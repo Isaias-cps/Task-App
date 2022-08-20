b9646b417055b255e3e3263123d76dcf8fb63c2a
Isaias_cps <isaias_cps@hotmail.com>
2022-08-20 02:28:06 -0300

    Configurando a tela de SplashFragment

--- app/src/main/java/com/example/myapplication/Ui/SplashFragment.kt
+++ app/src/main/java/com/example/myapplication/Ui/SplashFragment.kt
@@ -1,10 +1,14 @@
 package com.example.myapplication.Ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentSplashBinding
 
 
@@ -21,10 +25,20 @@ class SplashFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 3500)
+    }
+
+    private fun checkAuth(){
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+
+    }
+
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
