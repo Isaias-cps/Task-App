2b21254c6f4162cf1dc386a841bb323b5a6ad734
Isaias_cps <isaias_cps@hotmail.com>
2022-08-20 04:45:50 -0300

    Feito a ativação do binding na fragment_resgister

--- app/src/main/java/com/example/myapplication/Ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/myapplication/Ui/auth/RegisterFragment.kt
@@ -6,16 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentRegisterBinding
 
 
 class RegisterFragment : Fragment() {
 
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
