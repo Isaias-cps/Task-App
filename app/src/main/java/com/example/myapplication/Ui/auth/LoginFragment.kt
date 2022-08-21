69e1760be16daf196dee03725202655ac73d39b9
Isaias_cps <isaias_cps@hotmail.com>
2022-08-21 02:36:33 -0300

    Implementado as funcionalidade de Login e Register

--- app/src/main/java/com/example/myapplication/Ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/myapplication/Ui/auth/LoginFragment.kt
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentLoginBinding
 import com.example.myapplication.databinding.FragmentSplashBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,10 +33,14 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
+
         initClicks()
     }
 
     private fun initClicks(){
+        binding.btnLogin.setOnClickListener{validataData()}
+
         binding.btnRegister.setOnClickListener{
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
 
@@ -39,6 +50,40 @@ class LoginFragment : Fragment() {
             findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
 
         }
+
+    }
+
+    private fun validataData(){
+        val email = binding.edtEmail.text.toString().trim()
+        val password = binding.edtPassword.text.toString().trim()
+
+        if (email.isNotEmpty()){
+            if (password.isNotEmpty()){
+
+                binding.progressBar.isVisible = true
+
+                loginUser(email, password)
+
+            }else{
+                Toast.makeText(requireContext(),"Informe sua senha", Toast.LENGTH_SHORT).show()
+            }
+
+        }else{
+            Toast.makeText(requireContext(),"Informe seu Email", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun loginUser(email: String, password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                } else {
+                    binding.progressBar.isVisible = false
+                }
+
+            }
+
 
     }
 
