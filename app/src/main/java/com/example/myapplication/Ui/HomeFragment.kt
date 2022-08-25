715a882ea42740b956257b33d597507c59834bee
Isaias_cps <isaias_cps@hotmail.com>
2022-08-25 02:10:02 -0300

    Configuração da TabLayout, configuração do Firebase .auth, acrescentado a funcionalidade de retornar para o fragment de login

--- app/src/main/java/com/example/myapplication/Ui/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/Ui/HomeFragment.kt
@@ -5,15 +5,72 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
+import com.example.myapplication.Ui.adapter.ViewPagerAdapter
+import com.example.myapplication.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+
+        configTablayiut()
+
+        initClicks()
+    }
+
+    private fun initClicks(){
+        binding.ibLogout.setOnClickListener{logoutApp()}
+    }
+
+    private fun logoutApp(){
+        auth.signOut()
+        findNavController().navigate(R.id.action_homeFragment_to_authentication)
+
+    }
+
+    private fun configTablayiut(){
+        val adapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = adapter
+
+        adapter.addFragment(TodoFragment(), "Afazer")
+        adapter.addFragment(DoingFragment(), "fazendo")
+        adapter.addFragment(DoneFragment(), "Feitas")
+
+        binding.viewPager.offscreenPageLimit = adapter.itemCount
+
+        TabLayoutMediator(
+            binding.tabs, binding.viewPager
+        ) {tab, position ->
+            tab.text = adapter.getTitle(
+                position
+            )
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
