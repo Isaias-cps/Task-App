b153814d347a80d499d1fd1bba53c09fc7eb537e
Isaias_cps <isaias_cps@hotmail.com>
2022-08-25 07:12:26 -0300

    Criado o evento de click do FloatingActionButton

--- app/src/main/java/com/example/myapplication/Ui/TodoFragment.kt
+++ app/src/main/java/com/example/myapplication/Ui/TodoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentDoingBinding
 import com.example.myapplication.databinding.FragmentTodoBinding
@@ -21,6 +22,18 @@ class TodoFragment : Fragment() {
         _binding = FragmentTodoBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initClicks()
+    }
+
+    private fun initClicks(){
+        binding.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
     }
 
     override fun onDestroyView() {
